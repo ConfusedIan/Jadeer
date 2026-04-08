a7257df3f94527564160c862fb51f3e15ba03765
Layan Alghamdi <llaayann@gmail.com>
2026-04-08 10:39:35 +0300

    add services to start.bat

--- APIGateway/services/service_registry.py
+++ APIGateway/services/service_registry.py
@@ -5,6 +5,6 @@ SERVICES = {
     "assessment": os.getenv("ASSESSMENT_SERVICE_URL", "http://127.0.0.1:5003"),
     "cv": os.getenv("CV_SERVICE_URL", "http://127.0.0.1:5004"),
     "recommendation": os.getenv("RECOMMENDATION_SERVICE_URL", "http://127.0.0.1:5005"),
-    "ranking": os.getenv("RANKING_SERVICE_URL", "http://127.0.0.1:5005"),
     "certificates": os.getenv("CERT_SERVICE_URL", "http://127.0.0.1:5006"),
+    "ranking": os.getenv("RANKING_SERVICE_URL", "http://127.0.0.1:5007")
 }
